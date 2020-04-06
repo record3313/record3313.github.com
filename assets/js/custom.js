e8dd4b9c2a8e9f45757e0c23995d5278b23d1d68
greenviruss <record3313@naver.com>
2020-04-07 08:11:56 +0900

    main 모바일 작업

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	/*header*/
+	/* header fixed */
 	$(window).on("scroll", function () {
 		var ht = $(this).scrollTop();
 		
@@ -9,6 +9,49 @@ $(function () {
 		} else {
 			$("#header").addClass("on");
 		}
+	});
+	$(window).on("load",function(){
+
+		/* Desktop PC check */
+		var mobileArray = /Mobile|iPhone|od|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/;
+		if(!navigator.userAgent.match(mobileArray)){
+			$(".wrap").toggleClass("mob");
+		}
+		
+		/* PC browser resize */
+	    $(window).resize(function(){
+	    	$(".mob").each(function pcOnly(){
+			});
+	    })
+	    /* PC layout */
+	    $(".mob").each(function pcOnly(){
+
+		});
+
+		/* fitText */
+	    $.fn.fitText = function($kompressor, $option){
+	        var compressor = $kompressor || 1,
+
+	        setting = $.extend({
+	            "minFontSize":Number.NEGATIVE_INFINITY,
+	            "maxFontSize":Number.POSITIVE_INFINITY
+	        }, $option);
+	        
+	        return $(this).each(function(){
+	            var $this = $(this);
+
+	            var resizer = function(){
+	                var number = Math.max(Math.min($this.width() / (compressor*10), parseFloat(setting.maxFontSize)), parseFloat(setting.minFontSize));
+	                $this.css({
+	                    "font-size":number+"px",
+	                    "line-height":number+"px"
+	                })
+	            };
+	            resizer();
+	            $(window).on("resize.fittext orientationchange.fittext",resizer)
+	        })
+	    };
+	    $(".wrap").fitText(2,{minFontSize:"16px",maxFontSize:"32px"})
 	});
 
 	$('#container2').sliphover({
