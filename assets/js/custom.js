eca8c5c432737035e0ab1a923fda404322005b88
greenviruss <record3313@naver.com>
2020-04-07 08:58:56 +0900

    오탈자 확인

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,7 @@ $(function () {
 	$(window).on("scroll", function () {
 		var ht = $(this).scrollTop();
 		
-		if (ht <= 0) {
+		if (ht <= 40) {
 			$("#header").removeClass("on");
 		} else {
 			$("#header").addClass("on");
@@ -12,51 +12,33 @@ $(function () {
 	});
 	$(window).on("load",function(){
 
+
 		/* Desktop PC check */
 		var mobileArray = /Mobile|iPhone|od|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/;
 		if(!navigator.userAgent.match(mobileArray)){
 			$(".wrap").toggleClass("mob");
 		}
-		
-		/* PC browser resize */
-	    $(window).resize(function(){
-	    	$(".mob").each(function pcOnly(){
-			});
-	    })
-	    /* PC layout */
-	    $(".mob").each(function pcOnly(){
-
-		});
-
-		/* fitText */
-	    $.fn.fitText = function($kompressor, $option){
-	        var compressor = $kompressor || 1,
-
-	        setting = $.extend({
-	            "minFontSize":Number.NEGATIVE_INFINITY,
-	            "maxFontSize":Number.POSITIVE_INFINITY
-	        }, $option);
-	        
-	        return $(this).each(function(){
-	            var $this = $(this);
-
-	            var resizer = function(){
-	                var number = Math.max(Math.min($this.width() / (compressor*10), parseFloat(setting.maxFontSize)), parseFloat(setting.minFontSize));
-	                $this.css({
-	                    "font-size":number+"px",
-	                    "line-height":number+"px"
-	                })
-	            };
-	            resizer();
-	            $(window).on("resize.fittext orientationchange.fittext",resizer)
-	        })
-	    };
-	    $(".wrap").fitText(2,{minFontSize:"16px",maxFontSize:"32px"})
 	});
 
 	$('#container2').sliphover({
 		target : '.box',
 		caption : 'data-caption'
+	});
+
+	/* tooltip */
+	$(".tooltip").each(function(){
+		$(this).children(".btn-tooltip").on("click",function(){
+
+			$(this).next(".tooltip-pop").toggle();
+
+			if( $(this).next(".tooltip-pop").css("display") == "block" ){
+				$(".wrap").click(function(wrap){
+					if(!$(wrap.target).hasClass("btn-tooltip")){
+						$(".tooltip-pop").hide()
+					}
+				})
+		    }
+		})
 	});
 
 });
